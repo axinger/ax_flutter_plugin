fbd4fa389fb4722b3f5698df2c0ee18f9f4d0ce6
axinger <axingrun@outlook.com>
2021-03-06 18:33:31 +0800

    update…

--- lib/src/key_chain.dart
+++ lib/src/key_chain.dart
@@ -2,9 +2,10 @@ import 'dart:async';
 
 import 'package:ax_flutter_plugin/ax_flutter_plugin.dart';
 
-import 'package:flutter/material.dart';
 class KeyChain {
-  static Future<dynamic> get({@required String key}) {
+  static Future<dynamic> get({
+    required String key,
+  }) {
     return AxFlutterPlugin.channel.invokeMethod<dynamic>(
       'key_chain_get',
       <String, dynamic>{
@@ -13,8 +14,11 @@ class KeyChain {
     );
   }
 
-  static Future<bool> set({@required String key, @required dynamic value}) {
-    return AxFlutterPlugin.channel.invokeMethod<bool>(
+  static Future<bool?> set({
+    required String key,
+    required dynamic value,
+  }) {
+    return AxFlutterPlugin.channel.invokeMethod<bool?>(
       'key_chain_set',
       <String, dynamic>{
         'key': key,
@@ -23,7 +27,9 @@ class KeyChain {
     );
   }
 
-  static Future<dynamic> remove({@required String key}) {
+  static Future<dynamic> remove({
+    required String key,
+  }) {
     return AxFlutterPlugin.channel.invokeMethod<dynamic>(
       'key_chain_remove',
       <String, dynamic>{
