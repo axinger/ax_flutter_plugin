ed70dc6e7c0207fa2bbcf6481a651eafc5017a5d
axinger <liu_weixing@qq.com>
2022-09-24 20:32:58 +0800

    update…

---
+++ example/android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
@@ -0,0 +1,25 @@
+package io.flutter.plugins;
+
+import io.flutter.plugin.common.PluginRegistry;
+import com.ax.ax_flutter_plugin.AxFlutterPlugin;
+
+/**
+ * Generated file. Do not edit.
+ */
+public final class GeneratedPluginRegistrant {
+  public static void registerWith(PluginRegistry registry) {
+    if (alreadyRegisteredWith(registry)) {
+      return;
+    }
+    AxFlutterPlugin.registerWith(registry.registrarFor("com.ax.ax_flutter_plugin.AxFlutterPlugin"));
+  }
+
+  private static boolean alreadyRegisteredWith(PluginRegistry registry) {
+    final String key = GeneratedPluginRegistrant.class.getCanonicalName();
+    if (registry.hasPlugin(key)) {
+      return true;
+    }
+    registry.registrarFor(key);
+    return false;
+  }
+}
---
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<ax_flutter_plugin/AxFlutterPlugin.h>)
+#import <ax_flutter_plugin/AxFlutterPlugin.h>
+#else
+@import ax_flutter_plugin;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [AxFlutterPlugin registerWithRegistrar:[registry registrarForPlugin:@"AxFlutterPlugin"]];
+}
+
+@end
